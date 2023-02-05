04eb6254a2e69dd88a2e83420c972cd69fce0f23
b3at1 <sambederman@gmail.com>
2023-02-04 22:17:20 -0600

    started makepurchase functionality

--- storeFront.c
+++ storeFront.c
@@ -11,15 +11,17 @@
     make purchases, 
     view your receipt
 */  
-//map struct to make balls with name (key) and price (value)
+
+//map struct to make balls with name (key) and price (value) as well as a quantity
 struct map{
     char* key;
     int value;
     int quant;
 };
-
 struct map storeFront[MAPSIZE];
-void initmap(void){ //creates a map with random prices
+
+//populates the storeFront with random prices and quantities
+void initmap(void){ 
     srand(time(0));
     int i;
     /*
@@ -47,22 +49,33 @@ void initmap(void){ //creates a map with random prices
     
 }
 
-void prntMap(void){ //displays idx, name, and price
+//displays idx, name, and price
+void prntMap(void){ 
     int idx;
     for(idx = 0; idx < MAPSIZE; idx++){
+        //this seems like an awful way to concat stuff but I guess it works
         char *c = "c";
         char quant[50] = "";
         sprintf(quant, "%d", storeFront[idx].value);
-        strncat(quant, c, 49 - strlen(quant));       
+        strncat(quant, c, 49 - strlen(quant)); //we have 49-len(quant) space left because of \0
         printf("%-5d %-14s %-6s %3d\n", idx, storeFront[idx].key, quant, storeFront[idx].quant);
     }
 }
 
+//attempts to purchase item ID in respective quant, returns 0 if failed to purchase
+int makePurchase(int *wallet, int id, int quant){
+    storeFront[id].quant -= quant;
+    *wallet -= (quant * storeFront[id].value);
+    return 0;
+}
+
+void receipt();
 
 int main(void){
     initmap();
     srand(time(0)); //seed random
     int wallet = rand() % (30000 - 1000) + 1000 + 1;
+    int *wall = &wallet; //we need a pointer so it can be modified in methods
 
     printf("Welcome to the Ball Store!\nWe sell balls. You should buy them <3\n\n");
     printf("You have %dc in your wallet!\n", wallet);
@@ -80,9 +93,29 @@ int main(void){
 
 
     if(decide == 'y'){
-        printf("ID:   Name:%16s%13s\n", "Price:", "Quantity: ");
-        prntMap();  
-    }
+        while(decide == 'y'){
+            printf("ID:   Name:%16s%13s\n", "Price:", "Quantity: ");
+            prntMap();
+            printf("_______________________________________\n");
+            printf("Type in the ID of an item to purchase: ");
+
+            int item_id;
+            scanf("%1d", &item_id);
+
+            printf("Type in the quantity to purchase: ");
+            int item_quant;
+            scanf("%1d", &item_quant);
+
+            makePurchase(wall, item_id, item_quant); //make sure to pass in wall (the address), passing in wall* will derefence, meaning it passes in wallet
+            printf("You have %d left in your wallet.\n", wallet);
+
+            printf("Make another purchase? (y/n): ");
+            scanf("%1c", &decide);
+            printf("\n"); //flush stdout
+            decide = tolower(decide);
+            }
+        }
+
     else if(decide == 'n'){
         printf("bruh... you're making me cry :'(\n");
     }
